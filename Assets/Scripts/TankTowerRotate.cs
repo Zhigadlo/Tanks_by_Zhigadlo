0a3604421310d4f9c2f2dcf4dcd6af5173c0992a
Zhigadlo <vzhogol176@gmail.com>
2021-01-01 22:21:21 +0300

    Add speed to tower rotation

--- Assets/Scripts/TankTowerRotate.cs
+++ Assets/Scripts/TankTowerRotate.cs
@@ -4,15 +4,24 @@ namespace TankControll
 {
     public class TankTowerRotate : MonoBehaviour
     {
-        [SerializeField] private GameObject Tower;
+        [SerializeField] private Transform _transformObject;
+        [SerializeField] private float _towerRotateSpeed = 0.1f;
+        private Vector2 _currentDirection = new Vector3(0f, 1f, 0f);
 
+        private void Start()
+        {
+            _transformObject = this.transform;
+        }
         private void TowerRotate()
         {
-            float _rotZ;
-            Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
-            difference.Normalize();
-            _rotZ = Mathf.Atan2(difference.x, difference.y) * Mathf.Rad2Deg;
-            Tower.transform.rotation = Quaternion.Euler(0, 0, -_rotZ);
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 objectPos = _transformObject.position;
+
+            Vector2 direction = mousePos - objectPos;
+            direction.Normalize();
+
+            _currentDirection = Vector2.Lerp(_currentDirection, direction, _towerRotateSpeed);
+            _transformObject.up = _currentDirection;
         }
 
         private void FixedUpdate()
